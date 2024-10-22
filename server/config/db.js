123c8ead186f12a49e1104c1099837734f67aeae
SuyashGautam05 <gautamsuyash5@gmail.com>
2024-10-22 15:24:30 +0530

    remove about page due to problems

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,34 @@
 const mongoose = require('mongoose');
-const connectDB = async()=> {
+
+const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 100000, // 100 seconds for server selection
-      socketTimeoutMS: 100000, // Increase socket timeout as well
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      family: 4 // Use IPv4, skip trying IPv6
     });
+
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log('MongoDB Connection Error:', error.message);
-
+    console.error('MongoDB Connection Error:', error.message);
+    // Exit process with failure
+    process.exit(1);
   }
-}
+};
 
+// Handle connection events
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB');
+});
 
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
 
-module.exports = connectDB;
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
 
-
+module.exports = connectDB
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -4,7 +4,7 @@ const customerController = require('../controllers/customerController')
 
 
 router.get('/', customerController.homepage);
-router.get('/about', customerController.about);
+// router.get('/about', customerController.about);
 router.get('/add', customerController.addCustomer);
 router.post('/add', customerController.postCustomer);
 
